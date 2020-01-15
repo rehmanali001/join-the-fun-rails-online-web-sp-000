47df66566be56cf1675820d0360547f5b6a1b1a7
Rehman Ali <rehmanali001@gmail.com>
2020-01-15 21:30:31 +0000

    Automatically backed up by Learn

--- db/migrate/20191119003632_add_foreign_keys_to_rides.rb
+++ db/migrate/20191119003632_add_foreign_keys_to_rides.rb
@@ -1,7 +1,6 @@
-class AddForeignKeysToRides < ActiveRecord::Migration[5.0]
+class AddForeignKeysToRide < ActiveRecord::Migration[5.0]
   def change
-    change_table :rides do |t|
-      t.integer :taxi_id
-    end
+    add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
   end
 end
